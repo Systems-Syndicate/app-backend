3373b5d0420c6eff4a0e234b0f73b16a5552cd66
Kevin <104761532+Hasakev@users.noreply.github.com>
2024-09-05 10:30:57 +1000

    chore: Add calendar and upload-ics endpoints

--- api/app/views/routes.py
+++ api/app/views/routes.py
@@ -9,3 +9,30 @@ def health():
     return jsonify({
             "healthy":True
         }), 200
+
+### CALENDAR ENDPOINT #########################
+@api.route('/calendar', methods=['GET'])
+def get_calendar():
+    email = request.args.get('email')
+    calendar = db.Calendar()
+    return jsonify(calendar.get_all(email)), 200
+
+### UPLOAD ICS ENDPOINT #########################
+@api.route('/upload-ics', methods=['POST'])
+def upload_ics():
+    if 'file' not in request.files:
+        return jsonify({
+            "error": "No file part"
+        }), 400
+    
+    file = request.files['file']
+    print(file.filename)
+    if file.filename == '':
+        return jsonify({
+            "error": "No selected file"
+        }), 400
+    
+    ics_content = file.read().decode('utf-8')
+    return jsonify({
+        "content": ics_content
+    }), 200
